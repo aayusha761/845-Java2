c1c485847737dc89ddf6ad6995463269faa32415
Aayush Arora <aroraaayush132@gmail.com>
2020-08-06 14:51:29 +0530

    Merge Conflict1

--- OnlineExamApp/src/com/lti/controller/AnswerCheckServlet.java
+++ OnlineExamApp/src/com/lti/controller/AnswerCheckServlet.java
@@ -19,6 +19,7 @@ public class AnswerCheckServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hello");
 		HttpSession session = request.getSession();
 		Question qs = (Question) session.getAttribute("nextQs");
 		
